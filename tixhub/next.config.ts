1f4ecac0af6b83933d59cc06d06cd04224e7b23b
William Siong <w.siongsin@gmail.com>
2024-12-19 06:22:28 -0500

    changes

--- tixhub/next.config.ts
+++ tixhub/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/",
+      },
+    ];
+  },
 };
-
-export default nextConfig;
